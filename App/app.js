22e57760c0589953504567cf1afeafca3c208b63
Hercules <xyq160640@163.com>
2017-02-17 11:49:48 +0800

    try merge qiao3

--- App/app.js
+++ App/app.js
@@ -1,12 +1,25 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import VueRouter from  'vue-router';
+
 
 import App from "./app.vue";
-import Home from "./pages/home/home.vue";
-import Member from "./pages/member/member.vue";
-import Class from "./pages/class/class.vue";
-import Shop from "./pages/shop/shop.vue";
-import My from "./pages/my/my.vue";
+import Home from './pages/home/home.vue';
+import Member from './pages/member/member.vue';
+import Class from './pages/class/class.vue';
+import Shop from './pages/shop/shop.vue';
+import My from './pages/my/my.vue';
+import My_login from './pages/my/my_login.vue';
+import My_message from './pages/my/my_message.vue';
+import My_order from './pages/my/my_order.vue';
+import My_collection from './pages/my/my_collection.vue';
+import My_purse from './pages/my/my_purse.vue';
+import My_address from './pages/my/my_address.vue';
+import My_leaguer from './pages/my/my_leaguer.vue';
+import My_more from './pages/my/my_more.vue';
+import Mes_name from './pages/my/mes_name.vue';
+import Mes_contact from './pages/my/mes_contact.vue';
+import Contact_edit from './pages/my/contact_edit.vue';
+
 
 import Area from "./pages/home/area.vue";
 import Search from "./pages/home/search.vue";
@@ -27,59 +40,39 @@ import Food_detail from "./pages/home/life_food/food_detail.vue";
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-	// mode:"history",
-	routes:[
-		{path:"/",component:Home},
-		{path:"/member",component:Member},
-		{path:"/class",component:Class},
-		{path:"/shop",component:Shop},
-		{path:"/my",component:My},
-		{path:"/area",component:Area},
-		{path:"/search",component:Search},
-		{path:"/life_wonderful",component:Life_wonderful},
-		{path:"/life_food",component:Life_food},
-		{path:"/life_article",component:Life_article},
-		{path:"/Life_home",component:Life_home},
-		{path:"/life_service",component:Life_service},
-		{path:"/life_wonderful_estate",component:Life_wonderful_estate},
-		{path:"/food_detail",component:Food_detail},
-	]
+	// mode:'history',
+    routes:[
+      {path:"/",component:Home},
+      {path:"/member",component:Member},
+      {path:"/class",component:Class},
+      {path:"/shop",component:Shop},
+      {path:"/my",component:My},
+      {path:"/login",component:My_login},
+      {path:"/message",component:My_message},
+      {path:"/order",component:My_order},
+      {path:"/collection",component:My_collection},
+      {path:"/address",component:My_address},
+      {path:"/purse",component:My_purse},
+      {path:"/leaguer",component:My_leaguer},
+      {path:"/more",component:My_more},
+      {path:"/mes_name",component:Mes_name},
+      {path:"/mes_contact",component:Mes_contact}, 
+      {path:"/contact_edit",component:Contact_edit}, 
+      {path:"/area",component:Area},
+      {path:"/search",component:Search},
+      {path:"/life_wonderful",component:Life_wonderful},
+      {path:"/life_food",component:Life_food},
+      {path:"/life_article",component:Life_article},
+      {path:"/Life_home",component:Life_home},
+      {path:"/life_service",component:Life_service},
+      {path:"/life_wonderful_estate",component:Life_wonderful_estate},
+      {path:"/food_detail",component:Food_detail}
+    ]
 })
-
-
-// const router2 = new VueRouter({
-// 	{path:"/life_service",component:Life_service},
-// })
-
-//指定开始加载页面  
-// router.push("search");
-
+// 指定一开始加载的页面
+// router.push("class");
 
 new Vue({
-	router,
-	// router2,
-	render:h=>h(App),
+    router,
+    render:h=>h(App)
 }).$mount("#app")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
