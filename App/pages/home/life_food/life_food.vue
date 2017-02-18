0ca13aff7044f6dfa8cf946449010f542f7da432
lzwopen <453336456@qq.com>
2017-02-18 17:33:22 +0800

    home_change

--- App/pages/home/life_food/life_food.vue
+++ App/pages/home/life_food/life_food.vue
@@ -7,13 +7,13 @@
 		<div class="swiper-container">
 			<div class="swiper-wrapper big_img">
 		        <div class="swiper-slide">
-		        	<router-link to="/home"><img src="./img/food_big01.jpg"></router-link>
+		        	<router-link to="/"><img src="./img/food_big01.jpg"></router-link>
 		        </div>
 		        <div class="swiper-slide">
-		       		<router-link to="/home"><img src="./img/food_big02.jpg"></router-link>
+		       		<router-link to="/"><img src="./img/food_big02.jpg"></router-link>
 		        </div>
 		        <div class="swiper-slide">
-					<router-link to="/home"><img src="./img/food_big01.jpg"></router-link>
+					<router-link to="/"><img src="./img/food_big01.jpg"></router-link>
 		        </div>
 			    </div>
 		   <div class="swiper-pagination "></div>
@@ -23,7 +23,8 @@
 		<div>
 			<!-- 左边商家导航栏 -->
 			<ul class="left-side-bar">
-				<li v-for="item in items"><router-link to="/">{{item.con}}</router-link></li>
+				<li v-for="(item,index) in data" @click="show(index)">{{item.title}}</li>
+				<li class="line"></li>
 			</ul>
 
 			<!-- 右边详细信息 -->
@@ -50,6 +51,7 @@
 
 <script type="text/javascript">
 	import Header1 from '../../../component/header/header.vue';
+	import $ from '../../../common/js/jquery-3.1.1.min.js';
 
 	export default{
 		components:{
@@ -57,49 +59,208 @@
 		},
 		data(){
 			return{
-				items:[
-					{con:"传统糕点"},
-					{con:"膨化食品"},
-					{con:"无糖食品"},
-					{con:"休闲食品"},
-					{con:"燕麦食品"},
-					{con:"坚果炒货"},
-				],
-				msg:[
+				msg:"",
+				data:[
 					{
-						img:require("./img/food_small01.jpg"),
-						title:"薯条点心面休闲零食五味园  小包装500g",
-						currentPrice:"12.5",
-						originalCost:"15.8",
+						title:"传统糕点",
+						data:[
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"薯条点心面休闲零食五味园  小包装500g",
+								currentPrice:"12.5",
+								originalCost:"15.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"薯条点心面休闲零食五味园  小包装500g",
+								currentPrice:"12.5",
+								originalCost:"15.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"薯条点心面休闲零食五味园  小包装500g",
+								currentPrice:"12.5",
+								originalCost:"15.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"薯条点心面休闲零食五味园  小包装500g",
+								currentPrice:"12.5",
+								originalCost:"15.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"薯条点心面休闲零食五味园  小包装500g",
+								currentPrice:"12.5",
+								originalCost:"15.8",
+							},
+						]		
 					},
 					{
-						img:require("./img/food_small01.jpg"),
-						title:"薯条点心面休闲零食五味园  小包装500g",
-						currentPrice:"12.5",
-						originalCost:"15.8",
+						title:"膨化食品",
+						data:[
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"膨化食品面休闲零食五味园  小包装500g",
+								currentPrice:"13.5",
+								originalCost:"15.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"膨化食品面休闲零食五味园  小包装500g",
+								currentPrice:"12.5",
+								originalCost:"16.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"膨化食品面休闲零食五味园  小包装500g",
+								currentPrice:"12.5",
+								originalCost:"17.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"膨化食品面休闲零食五味园  小包装500g",
+								currentPrice:"12.5",
+								originalCost:"15.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"薯条点心面休闲零食五味园  小包装500g",
+								currentPrice:"12.5",
+								originalCost:"19.8",
+							},
+						]		
 					},
 					{
-						img:require("./img/food_small01.jpg"),
-						title:"薯条点心面休闲零食五味园  小包装500g",
-						currentPrice:"12.5",
-						originalCost:"15.8",
+						title:"无糖食品",
+						data:[
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"无糖食品面休闲零食五味园  小包装500g",
+								currentPrice:"13.5",
+								originalCost:"15.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"无糖食品面休闲零食五味园  小包装500g",
+								currentPrice:"13.5",
+								originalCost:"15.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"无糖食品面休闲零食五味园  小包装500g",
+								currentPrice:"13.5",
+								originalCost:"15.8",
+							},
+						]		
 					},
 					{
-						img:require("./img/food_small01.jpg"),
-						title:"薯条点心面休闲零食五味园  小包装500g",
-						currentPrice:"12.5",
-						originalCost:"15.8",
+						title:"休闲食品",
+						data:[
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"休闲食品面休闲零食五味园  小包装500g",
+								currentPrice:"13.5",
+								originalCost:"15.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"休闲食品面休闲零食五味园  小包装500g",
+								currentPrice:"13.5",
+								originalCost:"15.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"休闲食品面休闲零食五味园  小包装500g",
+								currentPrice:"13.5",
+								originalCost:"15.8",
+							},
+						]		
 					},
 					{
-						img:require("./img/food_small01.jpg"),
-						title:"薯条点心面休闲零食五味园  小包装500g",
-						currentPrice:"12.5",
-						originalCost:"15.8",
+						title:"燕麦食品",
+						data:[
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"燕麦食品面休闲零食五味园  小包装500g",
+								currentPrice:"13.5",
+								originalCost:"15.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"燕麦食品面休闲零食五味园  小包装500g",
+								currentPrice:"13.5",
+								originalCost:"15.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"燕麦食品面休闲零食五味园  小包装500g",
+								currentPrice:"13.5",
+								originalCost:"15.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"燕麦食品面休闲零食五味园  小包装500g",
+								currentPrice:"13.5",
+								originalCost:"15.8",
+							},
+							
+						]		
+					},
+					{
+						title:"坚果炒货",
+						data:[
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"坚果炒货面休闲零食五味园  小包装500g",
+								currentPrice:"11.5",
+								originalCost:"16.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"坚果炒货面休闲零食五味园  小包装500g",
+								currentPrice:"14.5",
+								originalCost:"15.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"坚果炒货面休闲零食五味园  小包装500g",
+								currentPrice:"13.5",
+								originalCost:"18.8",
+							},
+							{
+								img:require("./img/food_small01.jpg"),
+								title:"坚果炒货面休闲零食五味园  小包装500g",
+								currentPrice:"10.5",
+								originalCost:"11.8",
+							},
+							
+						]		
 					},
 				]
+
 			}
 		},
+		methods:{
+			show:function(i){
+				this.msg = this.data[i].data;
+				var lis = $(".left-side-bar li");
+				for (var j = 0; j < lis.length; j++) {
+					if(j == i){
+						lis.eq(j).css({"background":"#e6e6e6","color":"#f29004"})
+						$(".life_food .line").animate({
+							top:$(".life_food .line").height()*j + "px",
+						},200)
+					}else{
+						lis.eq(j).css({"background":"","color":""});
+					}
+				}
+
+			},
+		},
 		mounted:function(){
+			this.msg = this.data[0].data;
+			$(".left-side-bar li").eq(0).css({"background":"#e6e6e6","color":"#f29004"});
 			var mySwiper = new Swiper('.swiper-container',{
 				autoplay : 3000,
 				loop:true,
@@ -125,6 +286,7 @@
 		.left-side-bar{
 			width: 28%;
 			float: left;
+			position: relative;
 			li{
 				width: 100%;
 				height:60px;
@@ -137,7 +299,18 @@
 					color: black;
 				}
 			}
+
+			/*划线*/
+			.line{
+				width: 5%;
+				height: 61px;
+				background:#f29004; 
+				position: absolute;
+				top: 0;
+			}
 		}
+
+		/*右边详细信息*/
 		.information{
 			width: 72%;
 			float: right;
