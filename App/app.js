e3ef2ae2bc52248996e946fda0756411b607ac4b
Hercules <xyq160640@163.com>
2017-02-18 11:55:30 +0800

    merge qiao

--- App/app.js
+++ App/app.js
@@ -1,13 +1,20 @@
 import Vue from 'vue';
 import VueRouter from  'vue-router';
 
-import  App from "./app.vue";
+
+import App from "./app.vue";
 import Home from './pages/home/home.vue';
 import Member from './pages/member/member.vue';
 import Class from './pages/class/class.vue';
-import Shop from './pages/shop/shop.vue';
+// import Shop from './pages/shop/shop.vue';
+import Cart from "./pages/cart/cart.vue";
+
+import Login from "./pages/account/login.vue";
+import Register from "./pages/account/register.vue";
+import ChangePwd from "./pages/account/changePwd.vue";
+
 import My from './pages/my/my.vue';
-import My_login from './pages/my/my_login.vue';
+// import My_login from './pages/my/my_login.vue';
 import My_message from './pages/my/my_message.vue';
 import My_order from './pages/my/my_order.vue';
 import My_collection from './pages/my/my_collection.vue';
@@ -25,9 +32,30 @@ import Bill from './pages/my/purse_bill.vue';
 import Zhi_add from './pages/my/zhi_addmoney.vue';
 import Leag_pay from './pages/my/leag_pay.vue';
 
+import Area from "./pages/home/area.vue";
+import Search from "./pages/home/search.vue";
 
+//home的生活
+import Life_wonderful from "./pages/home/life_wonderful/life_wonderful.vue";
+import Life_food from "./pages/home/life_food/life_food.vue";
+import Life_article from "./pages/home/life_article/life_article.vue";
+import Life_home from "./pages/home/life_home/life_home.vue";
+import Life_service from "./pages/home/life_service/life_service.vue";
+
+//地产详细页
+import Life_wonderful_estate from "./pages/home/life_wonderful/life_wonderful_estate.vue";
+
+//商品详情
+import Food_detail from "./pages/home/life_food/food_detail.vue";
 
 Vue.use(VueRouter)
+
+const auth={
+  isLoggedIn:true,
+  account:"",
+  name:"",
+  phoneNumber:""
+}
 
 const router = new VueRouter({
 	// mode:'history',
@@ -35,9 +63,10 @@ const router = new VueRouter({
       {path:"/",component:Home},
       {path:"/member",component:Member},
       {path:"/class",component:Class},
-      {path:"/shop",component:Shop},
-      {path:"/my",component:My},
-      {path:"/login",component:My_login},
+      // {path:"/shop",component:Shop},
+      {path:"/cart",component:Cart,meta:{requiresAuth:true}},
+      {path:"/my",component:My,meta:{requiresAuth:true}},
+      {path:"/login",component:Login},
       {path:"/message",component:My_message},
       {path:"/order",component:My_order},
       {path:"/collection",component:My_collection},
@@ -48,16 +77,37 @@ const router = new VueRouter({
       {path:"/mes_name",component:Mes_name},
       {path:"/mes_contact",component:Mes_contact}, 
       {path:"/contact_edit",component:Contact_edit}, 
-      {path:"/changepass",component:Mes_changepass},
-      {path:"/changepurse",component:Mes_changepurse},
-      {path:"/offer",component:Offer},
-      {path:"/bill",component:Bill},
-      {path:"/zhi_add",component:Zhi_add},
-      {path:"/leag_pay",component:Leag_pay},
+      {path:"/area",component:Area},
+      {path:"/search",component:Search},
+      {path:"/life_wonderful",component:Life_wonderful},
+      {path:"/life_food",component:Life_food},
+      {path:"/life_article",component:Life_article},
+      {path:"/Life_home",component:Life_home},
+      {path:"/life_service",component:Life_service},
+      {path:"/life_wonderful_estate",component:Life_wonderful_estate},
+      {path:"/food_detail",component:Food_detail},
+      // {path:"/login",component:Login},
+      {path:"/register",component:Register},
+      {path:"/changePwd",component:ChangePwd}
     ]
 })
 // 指定一开始加载的页面
 // router.push("class");
+
+router.beforeEach((to,from,next)=>{
+  if (to.matched.some(record=>record.meta.requiresAuth)) {
+    if (!auth.isLoggedIn) {
+      next({
+        path:"/login",
+        query:{redirect:to.fullPath}
+      })
+    }else {
+      next()
+    }
+  }else{
+    next()
+  }
+})
 
 new Vue({
     router,
